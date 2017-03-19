92ac07e6d6e825309debd25d0a7f1c756d5809c1
吴柳燕 <wuliuyan@wuliuyans-MacBook-Pro.local>
2017-03-19 16:58:21 +0800

    Initial Commit

---
+++ adTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  adTest
+//
+//  Created by 吴柳燕 on 2017/3/19.
+//  Copyright © 2017年 吴柳燕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ adTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  adTest
+//
+//  Created by 吴柳燕 on 2017/3/19.
+//  Copyright © 2017年 吴柳燕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
